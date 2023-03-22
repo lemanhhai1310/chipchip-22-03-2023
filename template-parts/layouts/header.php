f5a15647605e28bd7ef98ba5c4dc5d867aa09fec
hailm <hai.lemanh.1310@gmail.com>
2023-03-22 19:55:55 +0700

    feat: init project

---
+++ template-parts/layouts/header.php
@@ -0,0 +1,21 @@
+<?php $projectName = "Chipchip Landingpage"; ?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="REFRESH" content="1800"/>
+    <title><?= /** @var TYPE_NAME  */
+        (isset($data['title'])) ? $data['title'] : ''; ?> - <?= $projectName ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <!--CSS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.10/dist/css/uikit.min.css" />
+    <link rel="stylesheet" href="style.css?v=<?php echo(time()) ?>">
+
+    <!--JS-->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.10/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.10/dist/js/uikit-icons.min.js"></script>
+    <script src="js/app.js?v=<?php echo(time()) ?>"></script>
+</head>
+<body class="">
+<!--app-->
+<div id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
